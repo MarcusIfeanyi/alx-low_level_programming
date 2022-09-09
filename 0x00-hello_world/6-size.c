3c47627ec84e2bc4c27554673e1e9b82598cd680
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-08 18:47:25 -0700

    little change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,12 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
 
-printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-    return (0);
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(float));
+
+    	return (0);
 }
-
