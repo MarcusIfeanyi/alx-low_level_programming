029df2c0edfdbfd8ce2f0d368f8c18adb3c0805e
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-09 15:31:51 -0700

    STRICT BETTY IF STATEMENT TASK

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n == 0 ) 
+if (n == 0)
 {
 printf("%i is zero\n", n);
 }
-else if (n < 0) 
+else if (n < 0)
 {
 printf("%i is negative\n", n);
 }
-else 
+else
 {
 printf("%i is positive\n", n);
 }
