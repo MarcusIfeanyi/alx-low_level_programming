7e6f5197b65143e7f8dea84bd23fd0366f599733
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-08 20:34:40 -0700

    ALX ADVANCED TASK

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
+/*
  * main - Entry point
- * Return 1 as success
- * Return: 0 if exited properly, non-zer otherwise
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	char tired[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, str1, 59);
 	return (1);
+
 }
