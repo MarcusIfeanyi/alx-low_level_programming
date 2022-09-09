c60ae13135cbca08446bae0e46c6ba7755c12778
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-09 15:23:42 -0700

    Strict C programming language TASK

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (seccess/correct)
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if ( n > 0 ) {
-		printf("%i is positive", n);
-	}
-	else if ( n == 0 ) {
-		printf("%i is zero\n", n);
-	}
-	else {
-		printf("%i is negative\n", n);
-	}
-	/* your code goes there */
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0 ) 
+{
+printf("%i is zero\n", n);
+}
+else if (n < 0) 
+{
+printf("%i is negative\n", n);
+}
+else 
+{
+printf("%i is positive\n", n);
+}
 	return (0);
 }
