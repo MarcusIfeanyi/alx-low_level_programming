40f9b251f92f9a61b6156da52571814c85418c1a
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-08 20:18:35 -0700

    night class work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return 1 as success
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zer otherwise
  */
 int main(void)
 {
-	write(2, " and that piece of art is useful\", - Dora Korpar, 2015-10-19\n,61);
+	write(2, " and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,59);
 	return (1);
 }
