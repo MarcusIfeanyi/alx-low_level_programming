4372b841920aabe2e6cf59b216d2f53c583cecf9
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-08 18:26:11 -0700

    C Programming Language Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+    return (0);
+}
+
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+ This project is all about solving task in ALX SE : C programming language
