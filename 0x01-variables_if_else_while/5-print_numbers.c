0423b638b3f884036f993e3141115cda6b952450
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-09 19:40:53 -0700

    loop through from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct) done by Marcus Ifeanyi
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%i", num);
+	}
+	printf("\n");
+
+	return (0);
+}
