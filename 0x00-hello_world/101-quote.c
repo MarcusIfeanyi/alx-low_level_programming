47e7e5e9151f610ea4da3ea98f7b64a812e2f7d9
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-12 03:01:32 -0700

    Use write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 1 (success)
  */
 int main(void)
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, str1, 59);
-return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
 
 }
 
