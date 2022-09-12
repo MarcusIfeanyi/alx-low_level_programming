ca59507a0a6be681088ce6ccc07a3d5ce976cb2e
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-12 04:37:51 -0700

    Double for loop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct) done by Marcus Ifeanyi
+ */
+int main(void)
+{
+	int num1;
+
+	int num2;
+
+	for (num1 = 48; num1 < 57; num1++)
+	{
+		for (num2 = 49; num2 < 57; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			putchar(44);
+			putchar(32);
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
