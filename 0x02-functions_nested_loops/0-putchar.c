f60dd66ce47476436131f41fc5610f6a14f6a3f5
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-13 13:23:13 -0700

    net task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -12,10 +12,10 @@ int main(void)
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(createStringFromPutchar[c]);
+		putchar(createStringFromPutchar[c]);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
