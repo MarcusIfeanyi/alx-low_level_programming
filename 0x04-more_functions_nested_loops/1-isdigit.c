ba920ced7e91070420196174f2d4bfd09e8d3bf9
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-16 13:44:55 -0700

    Check if an argument is a digit or not

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 #include "main.h"
 /**
  * _isdigit - Entry of the function
- *
+ *@c : parameter of the _isdigit and argument of the main script
  * Return: 0 The function returns 0 when it is successful
  * This  function checks if a number is a digit or not
  * Done by Marcus Ifeanyi
  */
- int _isdigit(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
