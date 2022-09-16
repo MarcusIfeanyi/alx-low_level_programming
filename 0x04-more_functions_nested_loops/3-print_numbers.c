5b06be3022739405d15383a817ac654251140513
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-16 14:38:36 -0700

    Print Numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry into the fxn
+ *
+ * Return: 0 for success
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n);
+}	
