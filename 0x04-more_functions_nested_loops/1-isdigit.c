21643a624c2d598448497bdb47568f72da952ee2
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-16 13:36:08 -0700

    Check if a value is a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - Entry of the function
+ *
+ * Return: 0 The function returns 0 when it is successful
+ * This  function checks if a number is a digit or not
+ * Done by Marcus Ifeanyi
+ */
+ int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
