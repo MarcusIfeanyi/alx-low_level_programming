9ecad7c5c090484245f03615a77aeba21398fba4
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-16 14:04:01 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - This function multiplies two parameters
+ * @a: This is the first parameter/argument
+ * @b: This is the second parameter/argument
+ * Return: 0 for the main and 'int' for this fxn
+ * This code was done by Marcus Ifeanyi
+ */
+int mul(int a, int b)
+{
+	num = a * b;
+
+	return (0);
+}
