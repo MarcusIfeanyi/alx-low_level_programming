db405dff04a179abe328c53651f25ddf0326feef
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-19 14:38:16 -0700

    Swap two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Name of the function that will perform the swap
+ * @a: The first variable to be swapped
+ * @b: The second variable to be swapped
+ * Return: void on success
+ * done by Marcus Ifeanyi
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	int d;
+
+	*a = c;
+
+	*b = d;
+
+	*a = d;
+
+	*b = c;
+}	
