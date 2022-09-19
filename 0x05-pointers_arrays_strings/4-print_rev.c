29a47e441b57a8ffd12d24c98c5bdd06aa0d64cb
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-19 15:55:09 -0700

    Print String arguement in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string followed by a new line..
+ *
+ * @s: the string to print.
+ * Return: void
+ * done by Marcus Ifeanyi
+ */
+
+void print_rev(char *s)
+{
+	int number_of_string;
+
+	number_of_string = 0;
+
+	while (s[number_of_string])
+	{
+		number_of_string += 1;
+	}
+		int num;
+
+	for (num = number_of_string - 1; num <= 0; num--)
+	{
+		_putchar(s[num]);
+	}
+	_putchar('\n');
+}
