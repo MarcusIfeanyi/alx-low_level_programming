10f189f0c3be8f811fdfa54c29a9047d1cfb7a83
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-19 14:24:09 -0700

    New Task on Pointers and Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - THIS IS  THE FUNCTION THAT WILL TAKE AN INTEGER
+ *@n: This is the parameter that will inturn be the arguement
+ * Return: void on success
+ * done by  Marcuss Ifeanyi.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This Repository is used to solve Task in ALX SE program on the topic: Pointers and Strings
