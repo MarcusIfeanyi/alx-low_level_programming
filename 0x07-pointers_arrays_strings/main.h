a7f2d58680dff351720cd7a256a1399c3a61bb40
root <mi.agbaji@stu.unizik.edu.ng>
2022-09-27 19:26:18 -0700

    Change the memory to a constant specified

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,23 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
-/**
- * File: holberton.h
- * Auth: Firdaus
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
