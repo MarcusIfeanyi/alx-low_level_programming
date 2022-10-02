3789899facc6fcf3b84b818382c8f0d4e976067e
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-02 15:46:10 -0700

    This has to do with using argument count and argument variable

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,12 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
 
-	for (count = 0; count <= argc; count++)
-	{
-
-	printf("%s\n", argv[count]);
-	}
+	printf("%s\n", argv[0]);
+	
 	return (0);
 }
