d5a5f2eb395f258a4b76fe5dd4e9ee541ce71594
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-02 17:21:00 -0700

    using the argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array String name
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	int slight_change;
+
+	slight_change = argc -1;
+
+	printf("%i\n", slight_change);
+	return (0);
+}
+
