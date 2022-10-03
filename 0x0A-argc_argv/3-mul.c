7e9359db6dd1a30e5796f3c6992c2b84245453f5
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-02 18:39:27 -0700

    Convert string to int using <stdlib.h> atoi()

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - Entry perform and action and return an int
  * @argc: Number of command line arguments
  * @argv: Array String name
- * Return: 1 on success
+ * Return: 0 on success and 1 otherwise
  */
 
 int main(int argc, char *argv[])
@@ -16,10 +16,11 @@ int main(int argc, char *argv[])
 	{
 		num = (atoi(argv[1])) * (atoi(argv[2]));
 		printf("%i\n", num);
+		return(0);
 	}
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
 }
