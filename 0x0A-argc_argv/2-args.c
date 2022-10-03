f7f3537d12cf115988b0d27b9cfe2de64e89abc7
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-02 17:46:31 -0700

    Use argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array String name
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+
+	for (index = 0; index <= argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+	return 0;
+}
