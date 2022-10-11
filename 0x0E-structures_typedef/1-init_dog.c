5e751dcbf55f921f02b8f094e50c22b8e948b018
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-10 17:35:01 -0700

    ALX Structure Task, dog as the object

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	name = d->name;
-	age = d->age;
-	owner = d->owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
