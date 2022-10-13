c6a9a838c8572f1b021c90db6bd237a937935752
root <mi.agbaji@stu.unizik.edu.ng>
2022-10-12 19:45:05 -0700

    Pointer To Function Task

---
+++ 0-print_name.c
@@ -0,0 +1,14 @@
+#include <function_pointers.h>
+#include <stdlib.h>
+/**
+ * print_name - fxn that prints a name
+ * @name: name of a person
+ * @f: pointer to a function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+f(name);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This Repository Is Used To Solve C Programming language Task
+This Repository Is Used To Solve Tasks On Function Pointers (0x0F-function_pointers)
